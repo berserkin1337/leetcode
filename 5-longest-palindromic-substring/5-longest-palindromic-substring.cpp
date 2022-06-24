1213430409ae889ac537536b764323fd93d5f05a
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-06-25 00:53:39 +0530

    Time: 1135 ms (9.63%), Space: 47.5 MB (26.73%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -11,13 +11,11 @@ public:
                     dp[i][j] =  (i==j+1)? true : dp[i-1][j+1];
                 }
                 else{
-                    //dp[i][j] = max(dp[i-1][j],dp[i][j+1]);
                     dp[i][j] = false;
                 }
             }
         }
         string curr = "";
-        
         for(int i=0;i<n;++i){
             for(int j=0;j<=i;++j){
                 
