5e6f33a9db2c3506fd7f493e00bdd1fc14a3228a
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-06-28 11:22:29 +0530

    Time: 77 ms (84.92%), Space: 15.2 MB (32.94%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -5,10 +5,11 @@ class Solution:
         dp[0][0] = triangle[0][0]
         for i in range(1,len(triangle)):
             for j in range(len(triangle[i])):
-                if j < len(triangle[i-1]) and j - 1 >= 0 :
-                    dp[i][j] = triangle[i][j] +  min(dp[i-1][j],dp[i-1][j-1])
-                elif j - 1 < 0:
-                    dp[i][j] = triangle[i][j] + dp[i-1][j]
-                elif j == len(triangle[i-1]):
-                    dp[i][j] = triangle[i][j] + dp[i-1][j-1]
+                # if j < len(triangle[i-1]) and j - 1 >= 0 :
+                #     dp[i][j] = triangle[i][j] +  min(dp[i-1][j],dp[i-1][j-1])
+                # elif j - 1 < 0:
+                #     dp[i][j] = triangle[i][j] + dp[i-1][j]
+                # elif j == len(triangle[i-1]):
+                #     dp[i][j] = triangle[i][j] + dp[i-1][j-1]
+                dp[i][j] = triangle[i][j] + min(dp[i-1][j] if j < len(triangle[i-1]) else float('inf') , dp[i-1][j-1] if j -1 >= 0 else float('inf') ) 
         return min(dp[-1])
