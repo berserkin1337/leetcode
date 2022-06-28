c5db2a642e4d2089852b4f21fc26273926e5b18f
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-06-28 12:35:45 +0530

    Time: 146 ms (10.99%), Space: 14.9 MB (59.49%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,15 +1,16 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp =[[0 for i in range(len(triangle[j]))] for j in range(len(triangle))]
+        dp= [0]*(len(triangle))
+        dp[0] = triangle[0][0]
         
-        dp[0][0] = triangle[0][0]
-        for i in range(1,len(triangle)):
-            for j in range(len(triangle[i])):
-                # if j < len(triangle[i-1]) and j - 1 >= 0 :
-                #     dp[i][j] = triangle[i][j] +  min(dp[i-1][j],dp[i-1][j-1])
-                # elif j - 1 < 0:
-                #     dp[i][j] = triangle[i][j] + dp[i-1][j]
-                # elif j == len(triangle[i-1]):
-                #     dp[i][j] = triangle[i][j] + dp[i-1][j-1]
-                dp[i][j] = triangle[i][j] + min(dp[i-1][j] if j < len(triangle[i-1]) else float('inf') , dp[i-1][j-1] if j -1 >= 0 else float('inf') ) 
-        return min(dp[-1])
+        for row in range(1,len(triangle)) :
+            for  i in range(len(triangle[row]) -1 ,-1,-1):
+                if i < len(triangle[row-1]) and i > 0:
+                    dp[i] = triangle[row][i] + min(dp[i],dp[i-1])
+                elif i == 0:
+                    dp[i] = triangle[row][i] + dp[i]
+                else:
+                    dp[i] = triangle[row][i] + dp[i-1]
+        return min(dp)
+        
+        
