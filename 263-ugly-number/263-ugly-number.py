765a8c3d076e38cede26670b6a9c61d7e17fadee
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-07-11 12:56:52 +0530

    Time: 61 ms (25.11%), Space: 13.9 MB (12.90%) - LeetHub

--- 263-ugly-number/263-ugly-number.py
+++ 263-ugly-number/263-ugly-number.py
@@ -1,24 +1,15 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        s = set()
         if n == 0:
             return 0
-        while n %2 == 0:
-            s.add(2)
-            n /= 2
         if n < 0:
             return False
-        if n == 0:
-            return 0
-        for i in range(3,int(math.sqrt(n)) + 1 , 2):
-            while n % i == 0:
-                s.add(i)
-                n /= i
-        
+        while n %2== 0:
+            n/=2
+        while n % 3 == 0:
+            n/=3
+        while n %5 == 0:
+            n/=5
         if n > 2:
-            s.add(n)
-        primes = [2,3,5]
-        for num in s :
-            if num not in primes:
-                return False
+            return False
         return True
