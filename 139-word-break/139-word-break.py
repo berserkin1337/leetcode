d0faa5db26aa0769dd301385005a253727325da5
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-07-20 15:00:57 +0530

    Time: 72 ms (29.23%), Space: 14.2 MB (14.33%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,20 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        global lookup
         global dictionary
         dictionary = set(wordDict)
-        lookup = set()
-
+        
+        @cache
         def func(s):
-            if s in lookup:
-                return False
             if len(s) == 0:
                 return True
             for idx in range(1, len(s)+1):
                 if s[:idx] in dictionary:
                     if func(s[idx:]):
                         return True
-            lookup.add(s)
-            print(len(lookup))
+
             return False
         return func(s)
