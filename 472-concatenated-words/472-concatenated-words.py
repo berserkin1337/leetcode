83c45ca5c8219f7c5d74005b4cfb7c14fffdb321
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-07-21 13:00:21 +0530

    Time: 8958 ms (5.04%), Space: 18 MB (71.49%) - LeetHub

---
+++ 472-concatenated-words/472-concatenated-words.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        def sortFunc(e):
+            return len(e)
+        words.sort(key=sortFunc)
+        # print(len(words))
+        res = []
+        preWords = words[::]
+        preWords.pop()
+        print(preWords)
+        print(len(words))
+        for i in range(len(words)-1,-1,-1):
+            if(self.wordBreak(words[i],preWords)):
+                res.append(words[i])
+            # print("hi")
+            if len(preWords)!=0:
+                preWords.pop()
+        return res
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+            global lookup
+            global dictionary
+            dictionary = set(wordDict)
+            lookup = set()
+
+            def func(s):
+                if s in lookup:
+                    return False
+                if len(s) == 0:
+                    return True
+                for idx in range(1, len(s)+1):
+                    if s[:idx] in dictionary:
+                        if func(s[idx:]):
+                            return True
+                lookup.add(s)
+                return False
+            return func(s)
