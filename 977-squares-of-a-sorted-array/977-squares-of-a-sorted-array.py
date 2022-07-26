49f89435c9172960abc28f748d284bd3821adb16
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-07-26 20:41:23 +0530

    Time: 404 ms (31.72%), Space: 16.4 MB (17.40%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,40 +1,14 @@
 class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        
-        def merge(arr1,arr2):
-            arr3 = [0]*(len(arr1) + len(arr2))
-            i,j = 0,0
-            curr = 0
-            while i < len(arr1) and j < len(arr2):
-                if arr1[i] < arr2[j]:
-                    arr3[curr] = arr1[i]
-                    i += 1
-                else:
-                    arr3[curr] = arr2[j]
-                    j+=1
-                curr += 1
-            if i < len(arr1):
-                while i < len(arr1):
-                    arr3[curr] = arr1[i]
-                    curr+=1
-                    i+=1
-            if j < len(arr2):
-                while j < len(arr2):
-                    arr3[curr] = arr2[j]
-                    curr+=1
-                    j+=1
-            # print(arr1,arr2) 
-            return arr3
+    def sortedSquares(self, A):
+        answer = [0] * len(A)
+        l, r = 0, len(A) - 1
+        while l <= r:
+            left, right = abs(A[l]), abs(A[r])
+            if left > right:
+                answer[r - l] = left * left
+                l += 1
+            else:
+                answer[r - l] = right * right
+                r -= 1
+        return answer
 
-        n = len(nums) 
-        if nums[0] >= 0:
-            return [num*num for num in nums]
-        i = 0
-        while i<n and nums[i]<0:
-            i += 1
-        neg = [-num for num in nums[:i]]
-        neg.reverse()
-        # print(neg)
-        arr3 = merge(neg,nums[i:])
-        # print(arr3)
-        return [num*num for num in arr3]
