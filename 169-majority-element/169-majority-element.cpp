1035426351ab3906f93c48f504d358a1c24366e5
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-07-29 17:35:48 +0530

    Time: 17 ms (94.11%), Space: 19.6 MB (12.88%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int,int> counter;
-        for(int i=0;i<nums.size();++i) {
-            counter[nums[i]]++;
-        }
-        
-        int max_element;
-        int max_num = INT_MIN;
-        for(auto it = counter.begin();it!= counter.end();++it){
-            if(it->second > max_num){
-                max_element = it->first;
-                max_num = it->second;
+        int candidate = nums[0],count = 1;
+        for(int i=1;i<nums.size();++i){
+            if(candidate==nums[i])count++;
+            else count --;
+            if(count == 0){
+                candidate = nums[i];
+                count = 1;
             }
         }
         
-        return max_element;
+        return candidate;
     }
 };
