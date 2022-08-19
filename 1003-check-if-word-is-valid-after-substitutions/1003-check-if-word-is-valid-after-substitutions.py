6032286cc81c0f73eb9c70eca368fb3be1830b01
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-08-20 00:03:49 +0530

    Time: 103 ms (52.06%), Space: 14.2 MB (53.90%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
@@ -1,14 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) == 0:
-            return True
-        if s == "abc":
-            return True
-        a = -1
-        for i in range(len(s)-2):
-            if s[i:i+3] == "abc":
-                a = i
-                break
-        if a == -1:
-            return False
-        return self.isValid(s[:i] + s[i+3:])
+        stack = []
+        
+        for char in s:
+            if char  == 'c':
+                if stack[-2:] != ['a','b']:
+                    return False
+                stack.pop()
+                stack.pop()
+            else:
+                stack.append(char)
+        return len(stack) == 0
