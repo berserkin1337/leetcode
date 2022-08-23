3a9075f73c459e27272362b4f113f46be3c5a7ff
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-08-23 20:20:29 +0530

    Time: 5923 ms (5.06%), Space: 13.9 MB (66.29%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -1,12 +1,25 @@
 class Solution:    
     def concatenatedBinary(self, n: int) -> int:
-        if n == 1:
-            return 1
-        msb = n
-        msb |= msb >> 1
-        msb |= msb >> 2
-        msb |= msb >> 4
-        msb |= msb >> 8
-        msb |= msb >> 16
-        msb += 1
-        return (self.concatenatedBinary(n-1)*(msb) + n)  % (10**9 + 7)
+        # if n == 1:
+        #     return 1
+        # msb = n
+        # msb |= msb >> 1
+        # msb |= msb >> 2
+        # msb |= msb >> 4
+        # msb |= msb >> 8
+        # msb |= msb >> 16
+        # msb += 1
+        # return (self.concatenatedBinary(n-1)*(msb) + n)  % (10**9 + 7)
+        ans   =  1
+        MOD = 10**9 + 7
+        for i in range(2,n+1):
+            msb = i
+            msb |= msb >> 1
+            msb |= msb >> 2
+            msb |= msb >> 4
+            msb |= msb >> 8
+            msb |= msb >> 16
+            msb += 1
+            ans = ans * msb + i
+            ans %= MOD
+        return ans
