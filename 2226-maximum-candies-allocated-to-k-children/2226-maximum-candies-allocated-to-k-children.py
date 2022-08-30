ce47e893edf985d9033e76cf061aa4af265e3f68
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-08-30 15:53:54 +0530

    Time: 3635 ms (6.74%), Space: 27.3 MB (65.22%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        n = len(candies)
+        if sum(candies) < k :
+            return 0
+        high = max(candies)
+        low = 1
+        
+        def isValid(num):
+            total  = 0
+            for i in range(len(candies)):
+                total += floor(candies[i]/num)
+            return total >= k
+        ans=  0
+        while low < high :
+            mid = (low + high) // 2
+            if isValid(mid):
+                low=  mid + 1
+                ans = max(ans, mid)
+            else:
+                high = mid - 1
+        mid = (low + high) // 2
+        if isValid(mid):
+            ans = max(ans,mid)
+        return ans 
