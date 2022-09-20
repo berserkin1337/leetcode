1f21f0c1b1bacbe9115b60ada8218f718e13403a
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-09-20 11:02:03 +0530

    Time: 7829 ms (23.78%), Space: 39.3 MB (35.91%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,13 +1,8 @@
-class Solution:
-    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        n,m = len(nums1),len(nums2)
-        dp = [[0 for i in range(m)] for j in range(n)]
-        
-        #dp[i][j] = max length of a repeated subarray that ends at pos i in first array , pos j in second array
-        
-        for i in range(n):
-            for j in range(m):
-                if nums1[i] == nums2[j] :
-                    dp[i][j] = 1 + (dp[i-1][j-1] if (i > 0 and j >0 ) else 0 )
-        return max(map(max, dp))  
-            
+class Solution(object):
+    def findLength(self, A, B):
+        memo = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+        for i in range(len(A) - 1, -1, -1):
+            for j in range(len(B) - 1, -1, -1):
+                if A[i] == B[j]:
+                    memo[i][j] = memo[i + 1][j + 1] + 1
+        return max(max(row) for row in memo)
