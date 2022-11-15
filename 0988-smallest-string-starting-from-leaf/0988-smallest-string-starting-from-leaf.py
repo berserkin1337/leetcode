b6487e448dc1c1576cef42948232d6cce3869641
berserkin1337 <91308890+berserkin1337@users.noreply.github.com>
2022-11-16 01:48:39 +0530

    Time: 57 ms (83.96%), Space: 15.6 MB (52.05%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -1,16 +1,24 @@
-class Solution(object):
-    def smallestFromLeaf(self, root):
-        self.ans = "~"
-
-        def dfs(node, A):
-            if node:
-                A.append(chr(node.val + ord('a')))
-                if not node.left and not node.right:
-                    self.ans = min(self.ans, "".join(reversed(A)))
-
-                dfs(node.left, A)
-                dfs(node.right, A)
-                A.pop()
-
-        dfs(root, [])
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        
+        self.ans  = ""
+        def dfs(root,curr="") :
+            if not root:
+                return
+            curr = chr(root.val + ord('a')) + curr
+            if root.left is None and root.right is None  :
+                if len(self.ans) :
+                    self.ans = min(self.ans ,  curr)
+                else:
+                    self.ans = curr
+            dfs(root.left,curr)
+            dfs(root.right,curr)
+            curr = curr[1:]
+        dfs(root)
         return self.ans
